6d70d4f128fb28d04fbba6a9545a4f4b2d79e3fc
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-10-21 19:09:33 -0300

    test: testa se não é possível fazer login sem o email

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -17,17 +17,36 @@ describe('Testes da rota /login', () => {
   describe('Verifica se é possível fazer login com sucesso', () => {
     before(async () => {
       chaiHttpResponse = await chai.request(app).post('/login').send({
-        email: 'teste@teste.com',
-        password: 'senhasupersecreta',
+        email: 'admin@admin.com',
+        password: 'secret_admin',
       });
     });
 
-    it('retorna status 200', async () => {
+    it('retorna status 200', () => {
       expect(chaiHttpResponse.status).to.be.equal(200);
     });
 
     it('retorna um token', () => {
       expect(chaiHttpResponse.body).to.have.property('token');
+    });
+  });
+
+  describe('Verifica se não é possível fazer login com o campo de email vazio', () => {
+    before(async () => {
+      chaiHttpResponse = await chai.request(app).post('/login').send({
+        email: '',
+        password: 'secret_admin',
+      });
+    });
+
+    it('retorna status 400', () => {
+      expect(chaiHttpResponse.status).to.be.equal(400);
+    });
+
+    it('retorna uma mensagem', () => {
+      expect(chaiHttpResponse.body).to.be.deep.equal({
+        message: 'All fields must be filled',
+      });
     });
   });
 });
