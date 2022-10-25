f607b4c558de0bcc545849998074db2943dbc655
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-10-25 15:49:43 -0300

    refactor: excluindo método para criptografar não utilizado no Bcrypt

--- app/backend/src/helpers/Bcryptjs.ts
+++ app/backend/src/helpers/Bcryptjs.ts
@@ -1,12 +1,6 @@
 import * as bcrypt from 'bcryptjs';
 
-export default class Bcrypt {
-  private static salt = 10;
-
-  public static encrypt(password: string): string {
-    return bcrypt.hashSync(password, Bcrypt.salt);
-  }
-
+export default abstract class Bcrypt {
   public static compare(password: string, encryptedPassword: string): boolean {
     return bcrypt.compareSync(password, encryptedPassword);
   }
--- app/backend/src/helpers/Token.ts
+++ app/backend/src/helpers/Token.ts
@@ -3,7 +3,7 @@ import * as jwt from 'jsonwebtoken';
 import { SignOptions, JwtPayload } from 'jsonwebtoken';
 import IPayload from '../interfaces/IPayload';
 
-export default class Token {
+export default abstract class Token {
   private static jwtSecret = process.env.JWT_SECRET || 'jwt_secret';
   private static jwtOptions: SignOptions = {
     algorithm: 'HS256',
@@ -12,11 +12,13 @@ export default class Token {
 
   public static create(payload: IPayload): string {
     const token = jwt.sign(payload, Token.jwtSecret, Token.jwtOptions);
+
     return token;
   }
 
   public static validate(token: string): JwtPayload {
     const payload = jwt.verify(token, Token.jwtSecret);
+
     return payload as JwtPayload;
   }
 }
